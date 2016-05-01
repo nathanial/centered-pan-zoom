cef55796cd5f8d9da2591fe26928141a69536477
Nathan Hartman <nathanial.hartman@gmail.com>
2016-05-01 03:12:11 -0700

    more panning

--- src/test.js
+++ src/test.js
@@ -32,4 +32,27 @@ describe('PanZoom', function() {
       assert.deepEqual(1, panner.scale);
     });
   });
+
+  describe("#pan() at scale x2", function(){
+    it("should pan right", function(){
+      var panner = new CenteredPanZoom({width: 100, height: 100, scale: 2});
+      panner.pan({start: {x: 0, y:0}, end: {x: 50, y:0}});
+      assert.deepEqual({x: 50, y: 0}, panner.translate);
+      assert.deepEqual(2, panner.scale);
+    });
+    it("should pan right multiple times", function(){
+      var panner = new CenteredPanZoom({width: 100, height: 100, scale: 2});
+      panner.pan({start: {x:0, y:0}, end: {x: 50, y:0}});
+      panner.pan({start: {x:0, y:0}, end: {x: 50, y:0}});
+      assert.deepEqual({x: 100, y:0}, panner.translate);
+      assert.deepEqual(2, panner.scale);
+    });
+    it("should pan down and right", function(){
+      var panner = new CenteredPanZoom({width: 100, height: 100, scale: 2});
+      panner.pan({start: {x:50,y:50}, end: {x:60, y: 60}});
+      assert.deepEqual({x:10, y:10}, panner.translate);
+      assert.deepEqual(2, panner.scale);
+    });
+  })
+
 });
